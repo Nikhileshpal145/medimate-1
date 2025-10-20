593ec18f8b6fbe88059067981cf6a52ae86a74bf
Nikhilesh Pal <nikhileshpal5@gmail.com>
2025-10-20 19:32:22 +0000

    Of course. It looks like we're still dealing with hydration errors, whic

--- src/components/main-nav.tsx
+++ src/components/main-nav.tsx
@@ -97,11 +97,11 @@ function MainNav({ pathname }: { pathname: string }) {
           </Collapsible>
         ) : (
           <SidebarMenuItem key={index}>
-            <Link href={item.href} passHref>
-              <SidebarMenuButton isActive={pathname === item.href}>
-                {item.icon}
-                <span>{item.label}</span>
-              </SidebarMenuButton>
+             <Link href={item.href} passHref>
+                <SidebarMenuButton isActive={pathname === item.href}>
+                    {item.icon}
+                    <span>{item.label}</span>
+                </SidebarMenuButton>
             </Link>
           </SidebarMenuItem>
         )
